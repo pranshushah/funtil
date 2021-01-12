4ad303880c10c4b28d599f6d30780be2c896ea7d
pranshu shah <pranshu.shah23@gmail.com>
2021-01-12 14:18:56 +0530

    union

---
+++ src/utils/union.ts
@@ -0,0 +1,17 @@
+import { concat } from './concat';
+import { partial2 } from './internals/partial2';
+import { unique } from './uniq';
+
+export function union<T extends any[]>(x1: T, x2: T): T;
+export function union<T extends any[]>(x1: T): (x2: T) => T;
+
+export function union<T extends any[]>(x1: T, x2?: T) {
+  return partial2(
+    function main(x1: T, x2: T) {
+      let result = concat(x1, x2);
+      return unique(result) as T;
+    },
+    x1,
+    x2
+  );
+}
