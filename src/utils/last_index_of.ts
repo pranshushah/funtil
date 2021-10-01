e21afff292afd90baccaf83ea63e15a5c62c98fa
pranshu shah <pranshu.shah23@gmail.com>
2021-10-01 12:51:28 +0530

    L example

--- src/utils/last_index_of.ts
+++ src/utils/last_index_of.ts
@@ -4,6 +4,10 @@ import { equals } from './math/equals';
 /**
  * @description returns last index of given element in array.returns -1 if no element is found.uses ```F.equals``` , also works with partial form.
  * @category List
+ * @example
+ * F.last_index_of({id:1,name:pranshu},[{id:1,name:"pranshu"},{id:2,name:"mit"},{id:1,name:"pranshu"}]) // 2
+ * F.last_index_of(2,[1,3,4,5]) // -1
+ * F.last_index_of("a",["a","b","c","a"]) // 3
  */
 export function last_index_of<T>(item: T, arr: readonly T[]): number;
 export function last_index_of<T>(item: T): (arr: readonly T[]) => number;
--- src/utils/length.ts
+++ src/utils/length.ts
@@ -1,6 +1,11 @@
 /**
  * @description returns length of given string or array.
  * @category List
+ * @example
+ * F.length("pranshu") // 7
+ * F.length([1,2,3,4]) // 4
+ * F.length([]) // 0
+ *
  */
 export function length<T>(xs: T[] | string) {
   return xs.length;
