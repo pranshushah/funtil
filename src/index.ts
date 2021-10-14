3fe2fd7199c572998fc3ccd49a107eb2cc5c78ad
pranshu shah <pranshu.shah23@gmail.com>
2021-10-14 13:37:00 +0530

    everything exported

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 export * from './utils/math';
+export * from './utils/Function';
+export * from './utils/List';
+export * from './utils/Object';
+export * from './utils/Logic';
+import F from './utils';
+
+export default F;
---
+++ src/utils/index.ts
@@ -0,0 +1,12 @@
+import * as MathFunctions from './math';
+import * as Functions from './Function';
+import * as LogicFunctions from './Logic';
+import * as ObjectFunctions from './Object';
+import * as ListFunction from './List';
+export default {
+  ...MathFunctions,
+  ...Functions,
+  ...LogicFunctions,
+  ...ObjectFunctions,
+  ...ListFunction,
+};
