2bc937a6756946b9db1026047639cd352cc01476
pranshu shah <pranshu.shah23@gmail.com>
2021-10-16 20:36:15 +0530

    docs readme

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ export * from './utils/Function';
 export * from './utils/List';
 export * from './utils/Object';
 export * from './utils/Logic';
-import F from './utils';
+// import F from './utils';
 
-export default F;
+// export default F;
